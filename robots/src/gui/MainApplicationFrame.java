f03ad59d5bcf4eb8945ebf0935061a7fb7f39dba
vVladEr <vlader.2004.ekb@gmail.com>
2025-03-11 09:58:15 +0500

    clean up

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -2,18 +2,9 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
-
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 import log.Logger;
 
 /**
@@ -66,95 +57,4 @@ public class MainApplicationFrame extends JFrame
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-
-    
-    
-//    protected JMenuBar createMenuBar() {
-//        JMenuBar menuBar = new JMenuBar();
-// 
-//        //Set up the lone menu.
-//        JMenu menu = new JMenu("Document");
-//        menu.setMnemonic(KeyEvent.VK_D);
-//        menuBar.add(menu);
-// 
-//        //Set up the first menu item.
-//        JMenuItem menuItem = new JMenuItem("New");
-//        menuItem.setMnemonic(KeyEvent.VK_N);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("new");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        //Set up the second menu item.
-//        menuItem = new JMenuItem("Quit");
-//        menuItem.setMnemonic(KeyEvent.VK_Q);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("quit");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        return menuBar;
-//    }
-    
-    // private JMenuBar generateMenuBar()
-    // {
-    //     JMenuBar menuBar = new JMenuBar();
-        
-    //     JMenu lookAndFeelMenu = new JMenu("Режим отображения");
-    //     lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
-    //     lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
-    //             "Управление режимом отображения приложения");
-        
-    //     {
-    //         JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-    //         systemLookAndFeel.addActionListener((event) -> {
-    //             setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    //             this.invalidate();
-    //         });
-    //         lookAndFeelMenu.add(systemLookAndFeel);
-    //     }
-
-    //     {
-    //         JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-    //         crossplatformLookAndFeel.addActionListener((event) -> {
-    //             setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-    //             this.invalidate();
-    //         });
-    //         lookAndFeelMenu.add(crossplatformLookAndFeel);
-    //     }
-
-    //     JMenu testMenu = new JMenu("Тесты");
-    //     testMenu.setMnemonic(KeyEvent.VK_T);
-    //     testMenu.getAccessibleContext().setAccessibleDescription(
-    //             "Тестовые команды");
-        
-    //     {
-    //         JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-    //         addLogMessageItem.addActionListener((event) -> {
-    //             Logger.debug("Новая строка");
-    //         });
-    //         testMenu.add(addLogMessageItem);
-    //     }
-
-    //     menuBar.add(lookAndFeelMenu);
-    //     menuBar.add(testMenu);
-    //     return menuBar;
-    // }
-    
-    // private void setLookAndFeel(String className)
-    // {
-    //     try
-    //     {
-    //         UIManager.setLookAndFeel(className);
-    //         SwingUtilities.updateComponentTreeUI(this);
-    //     }
-    //     catch (ClassNotFoundException | InstantiationException
-    //         | IllegalAccessException | UnsupportedLookAndFeelException e)
-    //     {
-    //         // just ignore
-    //     }
-    // }
-    
 }
