47078a01df04a81667df627175f2735614042b32
vVladEr <vlader.2004.ekb@gmail.com>
2025-03-28 18:22:59 +0500

    move robot logic away from visualizer

---
+++ robots/src/logic/RobotData.java
@@ -0,0 +1,14 @@
+package logic;
+
+public final class RobotData {
+    public final int x;
+    public final int y; 
+    public final double direction; 
+
+    public RobotData(int x, int y, double direction)
+    {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
---
+++ robots/src/logic/RobotLogic.java
@@ -0,0 +1,139 @@
+package logic;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+public class RobotLogic {
+
+    private volatile double m_robotPositionX = 100;
+    private volatile double m_robotPositionY = 100; 
+    private volatile double m_robotDirection = 0; 
+    private volatile Dimension windowSize;
+
+    private volatile int m_targetPositionX = 150;
+    private volatile int m_targetPositionY = 100;
+    
+    private static final double maxVelocity = 0.1; 
+    private static final double maxAngularVelocity = 0.001; 
+ 
+
+    public void setTargetPosition(Point p)
+    {
+        m_targetPositionX = p.x;
+        m_targetPositionY = p.y;
+    }
+
+    public void setRobotPosition(RobotData robot)
+    {
+        m_robotPositionX = robot.x;
+        m_robotPositionY = robot.y;
+        m_robotDirection = robot.direction;
+    }
+
+    public Point getTargetPosition()
+    {
+        return new Point(m_targetPositionX, m_targetPositionY);
+    }
+
+    public RobotData getRobot()
+    {
+        return new RobotData(round(m_robotPositionX), round(m_robotPositionY), m_robotDirection);
+    }
+
+
+    private static double distance(double x1, double y1, double x2, double y2)
+    {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+    
+    private static double angleTo(double fromX, double fromY, double toX, double toY)
+    {
+        double diffX = toX - fromX;
+        double diffY = toY - fromY;
+        
+        return asNormalizedRadians(Math.atan2(diffY, diffX));
+    }
+    
+    public void update(Dimension newWindonsize)
+    {
+        windowSize = newWindonsize;
+        double distance = distance(m_targetPositionX, m_targetPositionY, 
+            m_robotPositionX, m_robotPositionY);
+        if (distance < 0.5)
+        {
+            return;
+        }
+        double velocity = maxVelocity;
+        double angleToTarget = angleTo(m_robotPositionX, m_robotPositionY, m_targetPositionX, m_targetPositionY);
+        double angularVelocity = 0;
+        if (angleToTarget > m_robotDirection)
+        {
+            angularVelocity = maxAngularVelocity;
+        }
+        if (angleToTarget < m_robotDirection)
+        {
+            angularVelocity = -maxAngularVelocity;
+        }
+        
+        moveRobot(velocity, angularVelocity, 10);
+    }
+    
+    private static double applyLimits(double value, double min, double max)
+    {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+    
+    private void moveRobot(double velocity, double angularVelocity, double duration)
+    {
+        velocity = applyLimits(velocity, 0, maxVelocity);
+        angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+        double newX = m_robotPositionX + velocity / angularVelocity * 
+            (Math.sin(m_robotDirection  + angularVelocity * duration) -
+                Math.sin(m_robotDirection));
+        if (!Double.isFinite(newX))
+        {
+            newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
+        }
+        double newY = m_robotPositionY - velocity / angularVelocity * 
+            (Math.cos(m_robotDirection  + angularVelocity * duration) -
+                Math.cos(m_robotDirection));
+        if (!Double.isFinite(newY))
+        {
+            newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
+        }
+        if (newX > windowSize.width || newX < 0
+            || newY > windowSize.height || newY < 0)
+            {
+                m_robotDirection = asNormalizedRadians(m_robotDirection + Math.PI);
+                return;
+            }
+        m_robotPositionX = newX;
+        m_robotPositionY = newY;
+        double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration); 
+        m_robotDirection = newDirection;
+    }
+
+    private static double asNormalizedRadians(double angle)
+    {
+        while (angle < 0)
+        {
+            angle += 2*Math.PI;
+        }
+        while (angle >= 2*Math.PI)
+        {
+            angle -= 2*Math.PI;
+        }
+        return angle;
+    }
+    
+    private static int round(double value)
+    {
+        return (int)(value + 0.5);
+    }
+}
