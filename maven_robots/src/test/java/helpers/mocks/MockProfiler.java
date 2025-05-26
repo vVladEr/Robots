cc090bafc41748188f5ce591bab1b180c4a53d35
vVladEr <vlader.2004.ekb@gmail.com>
2025-05-26 22:38:01 +0500

    final merge

---
+++ maven_robots/src/test/java/helpers/mocks/MockProfiler.java
@@ -0,0 +1,38 @@
+package helpers.mocks;
+
+import maven_robots.data.profiler.IProfiler;
+import maven_robots.gui.frames.baseFrames.FrameState;
+
+public class MockProfiler implements IProfiler {
+
+    @Override
+    public void saveFrameState(FrameState frameState) {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+    @Override
+    public void loadFrameState(FrameState frameState) {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+    @Override
+    public void setProfileName(String profileName) {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+    @Override
+    public void saveLanguage() {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+    @Override
+    public void loadLanguage() {
+        // TODO Auto-generated method stub
+        return;
+    }
+
+}
